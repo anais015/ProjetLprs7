3d70dfc01776dc63a9d0a43b4cc9fab9afe46746
MAIGNAN_Ky <k.maignan@lprs.fr>
2022-09-12 11:56:59 +0200

    Ajout de la classe Type.php

---
+++ src/administrateur/Type.php
@@ -0,0 +1,70 @@
+<?php
+
+class Type
+{
+    private $idtype;
+    private $nom;
+    private $refadmin;
+
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+    }
+
+    protected function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdtype()
+    {
+        return $this->idtype;
+    }
+
+    /**
+     * @param mixed $idtype
+     */
+    public function setIdtype($idtype)
+    {
+        $this->idtype = $idtype;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRefadmin()
+    {
+        return $this->refadmin;
+    }
+
+    /**
+     * @param mixed $refadmin
+     */
+    public function setRefadmin($refadmin)
+    {
+        $this->refadmin = $refadmin;
+    }
+}
