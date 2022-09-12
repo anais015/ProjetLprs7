bc4c8a579c1e4b8830f2bf898c4c67dda7393456
HaimyLe <H.le@lprs.fr>
2022-09-12 11:46:37 +0200

    ajout de CRUD et refactor de l'attribut $password en $mot_de_passe

--- src/etudiant/Etudiant.php
+++ src/etudiant/Etudiant.php
@@ -31,4 +31,55 @@ class Etudiant extends Utilisateur
             else return false;
         }
     }
+
+    public function connexion($bdd){
+        $sql='SELECT * FROM etudiant WHERE email=:email AND mot_de_passe=:mot_de_passe AND valide=1';
+        $request = $bdd->prepare($sql);
+        $execute = $request->execute(array(
+            'email'=>$this->email,
+            'mot_de_passe'=>$this->mot_de_passe
+        ));
+        if ($execute){
+            $result=$request->fetch();
+            if(is_array($result)) return $result;
+            else return false;
+        }
+        else return false;
+    }
+
+    public function modification ($bdd){
+        $sql = 'UPDATE etudiant SET nom =:nom, prenom=:prenom, email=:email, domaine_etude=:domaine_etude WHERE  id_etudiant =:id_etudiant';
+        $request = $bdd->prepare($sql);
+        $execute=$request->execute(array(
+            'id_etudiant'=>$this->id_etudiant,
+            'nom'=> $this->nom,
+            'prenom'=> $this->prenom,
+            'email'=> $this->email,
+            'domaine_etude'=> $this->domaine_etude
+        ));
+        if($execute) return true;
+        else return false;
+    }
+
+    private function verificationPassword ($bdd)
+    {
+        $sql = 'SELECT mot_de_passe FROM etudiant WHERE email = :email ';
+        $request = $bdd->prepare($sql);
+        $execute = $request->execute(array('email' => $this->email));
+        if ($execute) {
+            $result = $request->fetch();
+            if (is_array($result)) return true;
+        } else return false;
+    }
+
+    public function modificationPassword ($bdd){
+        if ($this->verificationPassword($bdd)){
+            $sql = 'UPDATE etudiant SET mot_de_passe =:mot_de_passe WHERE  id_etudiant =:id_etudiant';
+            $request = $bdd->prepare($sql);
+            $execute=$request->execute(array('mot_de_passe'=>$this->mot_de_passe));
+            if ($execute) return true;
+            else return false;
+        }
+        else return false;
+    }
 }
