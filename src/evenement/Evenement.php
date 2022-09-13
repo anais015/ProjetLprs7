a372a20053e2bbe92a371980ace0050c94b477a1
HaimyLe <H.le@lprs.fr>
2022-09-13 07:41:28 +0200

    ajout d'une classe Evenement

---
+++ src/evenement/Evenement.php
@@ -0,0 +1,138 @@
+<?php
+
+class Evenement
+{
+    private $nom;
+    private $description;
+    private $date;
+    private $heure;
+    private $duree;
+    private $valide;
+    private $refSalle;
+    private $refEntreprise;
+    private $refEtudiant;
+    private $refAdmin;
+
+    public function __construct(array $donnees)
+    {
+        $this->hydrate($donnees);
+    }
+
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function setNom($nom): void {
+        $this->nom = $nom;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description): void {
+        $this->description = $description;
+    }
+
+    public function getDate() {
+        return $this->date;
+    }
+
+    public function setDate($date): void {
+        $this->date = $date;
+    }
+
+    public function getHeure() {
+        return $this->heure;
+    }
+
+    public function setHeure($heure): void {
+        $this->heure = $heure;
+    }
+
+    public function getDuree() {
+        return $this->duree;
+    }
+
+    public function setDuree($duree): void {
+        $this->duree = $duree;
+    }
+
+    public function getValide() {
+        return $this->valide;
+    }
+
+    public function setValide($valide): void {
+        $this->valide = $valide;
+    }
+
+    public function getRefSalle() {
+        return $this->refSalle;
+    }
+
+    public function setRefSalle($refSalle): void {
+        $this->refSalle = $refSalle;
+    }
+
+    public function getRefEntreprise() {
+        return $this->refEntreprise;
+    }
+
+    public function setRefEntreprise($refEntreprise): void {
+        $this->refEntreprise = $refEntreprise;
+    }
+
+    public function getRefEtudiant() {
+        return $this->refEtudiant;
+    }
+
+    public function setRefEtudiant($refEtudiant): void {
+        $this->refEtudiant = $refEtudiant;
+    }
+
+    public function getRefAdmin() {
+        return $this->refAdmin;
+    }
+
+    public function setRefAdmin($refAdmin): void {
+        $this->refAdmin = $refAdmin;
+    }
+
+    public function etudiantOrganiseEvenement ($bdd){
+        $sql ='SELECT * FROM evenement WHERE date=:date AND heure=:heure AND ref_etudiant=:ref_etudiant';
+        $request = $bdd->prepare($sql);
+        $execute = $request->execute(array(
+            'date'=> $this->date,
+            'heure'=> $this->heure,
+            'ref_etudiant'=>$this->refEtudiant));
+        if($execute) {
+            $result = $request->fetch();
+            if (is_array($result)) return false;
+        }
+        else {
+            $sql='INSERT INTO evenement (nom, description, date, heure, duree) VALUES (:nom, :description, :date, :heure, :duree)';
+            $request = $bdd->prepare($sql);
+            $execute=$request->execute(array(
+                'nom'=> $this->nom,
+                'description'=> $this->description,
+                'date'=> $this->date,
+                'heure'=> $this->heure,
+                'duree'=> $this->duree,
+                'ref_etudiant'=>$this->refEtudiant
+            ));
+            if($execute) return true;
+            else return false;
+        }
+    }
+
+}
