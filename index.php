25e1936dbfd7f40dd26ad395ba15af55b4f4a7e2
HaimyLe <H.le@lprs.fr>
2022-09-19 09:26:23 +0200

    ajout des pages HTML

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lycée Robert Schuman</title>
+</head>
+<body>
+<nav>
+    <div class="bottom-row">
+        <a href="">Accueil</a>
+        <a href="connexion.php">Se Connecter</a>
+        <a href="inscription.php">S'inscrire</a>
+        <a href="contact.php">Contact</a>
+    </div>
+</nav>
+
+</body>
+</html>
