6039974cee91b614fc53f84db1b19bab0060e886
haimy <h.le@lprs.fr>
2022-09-26 23:36:53 +0200

    ajout d'une page d'accueil etudiant

---
+++ src/view/etudiant/accueil.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inscription</title>
+</head>
+<body>
+<nav>
+    <div class="bottom-row">
+        <a href="../../index.php">Accueil</a>
+        <a href="connexion.php">Trouver un job</a>
+        <a href="inscription.php">Trouver un événement</a>
+        <a href="inscription.php">Organizer un événement</a>
+        <a href="#">Contact</a>
+
+        <a href="connexion.php">Mes événement</a>
+        <a href="inscription.php">Mes candidatures</a>
+        <a href="inscription.php">Mes rendez-vous</a>
+        <a href="connexion.php">Mon compte</a>
+        <a href="inscription.php">Se déconnecter</a>
+    </div>
+</nav>
+
+
+</body>
+</html>
+
