7f409098a5d9e3c8f6da8a0fb776558184a8bfb1
haimy <h.le@lprs.fr>
2022-09-26 23:48:56 +0200

    ajout la gestion d'erreur

--- src/traitement/etudiant/traitementConnexion.php
+++ src/traitement/etudiant/traitementConnexion.php
@@ -4,6 +4,8 @@ require_once "../../model/Utilisateur.php";
 require_once "../../model/etudiant/Etudiant.php";
 require_once "../../model/Connexion.php";
 
+$erreur=false;
+
 $cnx = new Bdd();
 $bdd = $cnx->getBdd();
 //var_dump($_POST);
@@ -22,3 +24,38 @@ if(isset($_POST['connexion'])) {
 //    if (!$inscription) $erreur=true;
 //    var_dump($erreur);
 }
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+    <div class="bottom-row">
+        <a href="../../../index.php">Accueil</a>
+        <a href="../../view/connexion.php">Se Connecter</a>
+        <a href="../../view/inscription.php">S'inscrire</a>
+        <a href="#">Contact</a>
+    </div>
+</nav>
+<div class="container">
+    <div class="container" id="alert"
+    <?php
+    if (!$erreur) echo 'style="display:none;"';
+    else echo 'style="display:block; background-color:#f8bdc1; text-align: center"';
+    ?>
+    <input type="hidden"> &#9888; Erreur : Veuillez réessayer.
+    <a href="../../view/connexion.php">Revenir à la page de connexion</a>
+</div>
+<div class="container" id="alert"
+    <?php
+    if (!$connexion) header("location:../../view/etudiant/accueil.php")
+    ?>
+
+</div>
+</body>
+</html>
