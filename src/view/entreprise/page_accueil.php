4ce3d256d64871b84a696588ea3e3604c96bd4b5
anais015 <A.LEMAIRE@lprs.fr>
2022-10-03 12:05:52 +0200

    ajout de la nav-barre

--- src/view/entreprise/page_accueil.php
+++ src/view/entreprise/page_accueil.php
@@ -1,8 +1,33 @@
-<html>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Page d'accueil pour entreprise </title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <style>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
 
-<h2> Page d'accueil pour test de redirection </h2>
+        li {
+            display: inline;
+        }
+    </style>
+</head>
+<body>
+
+<ul>
+    <li><a href="page_accueil.php">Accueil</a></li>
+    <li><a href="modifProfil.php">Modification Profil</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">Déconnexion</a></li>
+</ul>
+
+<h2> Page d'accueil </h2>
 <h4> ... même si elle est moche ! </h4>
 
-
-
+</body>
 </html>
