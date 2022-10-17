5a3866923e64f9d463808c17cb52ddcfbca57653
anais015 <A.LEMAIRE@lprs.fr>
2022-10-17 15:13:55 +0200

    création de la page événement

---
+++ src/view/entreprise/evenement.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Page des événements</title>
+    <link rel="stylesheet" href="../../style/styleEntreprise.css">
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="page_accueil.php">Accueil</a></li>
+        <li><a href="creerEventEntreprise.php">Créer un événement</a></li>
+        <li><a href="../contact.php">Contact</a></li>
+        <li><a href="deconnexion.php">Déconnexion</a></li>
+    </ul>
+</nav>
+
+<h2> Page des événements </h2>
+<h4> ... même si elle est moche ! &#128521;</h4>
+
+</body>
+</html>
