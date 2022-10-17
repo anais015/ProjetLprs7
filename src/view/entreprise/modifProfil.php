ebf0cd28c7bfb5956e353e38bcf51f73e2bdbc61
anais015 <A.LEMAIRE@lprs.fr>
2022-10-17 10:42:27 +0200

    ajout de code html

--- src/view/entreprise/modifProfil.php
+++ src/view/entreprise/modifProfil.php
@@ -21,10 +21,33 @@
 
 <ul>
     <li><a href="page_accueil.php">Accueil</a></li>
-    <li><a href="#">Déconnexion</a></li>
+    <li><a href="deconnexion.php">Déconnexion</a></li>
 </ul>
 
+<form action='../../traitement/entreprise/traitementModifProfilEntreprise.php' method='POST'>
+    <label for='nom'><b>Nom</b></label>
+    <input type='text' placeholder='Nom' name='nom'  required>
 
+    <label for='prenom'><b>Prénom</b></label>
+    <input type='text' placeholder='Prénom' name='prenom' required>
+
+    <label for='role_entreprise'><b>Rôle de la société</b></label>
+    <input type='text' placeholder='Rôle' name='role_societe' required>
+
+    <label for='nom_entreprise'><b>Nom Entreprise</b></label>
+    <input type='text' placeholder='Nom de l Entreprise' name='nom_entreprise'  required>
+
+    <label for='rue_entreprise'><b>Rue</b></label>
+    <input type='text' placeholder='Rue' name='rue_entreprise'  required>
+
+    <label for='ville_entreprise'><b>Ville</b></label>
+    <input type='text' placeholder='Ville' name='ville_entreprise'  required>
+
+    <label for='cp_entreprise'><b>Code Postal</b></label>
+    <input type='text' placeholder='Code Postal' name='cp_entreprise'  required>
+
+    <input type='submit' name='modifier' id='modification'>
+</form>
 
 </body>
 </html>
