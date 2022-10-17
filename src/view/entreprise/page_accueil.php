30fba44e3b4185a1df1608fe6d02ec788085cbd8
anais015 <A.LEMAIRE@lprs.fr>
2022-10-17 11:54:07 +0200

    modification de la nav-barre

--- src/view/entreprise/page_accueil.php
+++ src/view/entreprise/page_accueil.php
@@ -3,25 +3,14 @@
 <head>
     <meta charset="utf-8">
     <title>Page d'accueil pour entreprise </title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
-    <style>
-        ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-        }
-
-        li {
-            display: inline;
-        }
-    </style>
+    <link rel="stylesheet" href="../../style/styleEntreprise.css">
 </head>
 <body>
 
 <ul>
     <li><a href="page_accueil.php">Accueil</a></li>
-    <li><a href="modifProfil.php">Modification Profil</a></li>
+    <li><a href="modifProfil.php">Modification de profil</a></li>
+    <li><a href="creerEventEntreprise.php">Créer un événement</a></li>
     <li><a href="../contact.php">Contact</a></li>
     <li><a href="#">Déconnexion</a></li>
 </ul>
