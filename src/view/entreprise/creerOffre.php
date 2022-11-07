68d38c9b938429beef23842a5e31c9ebbb40cac5
anais015 <A.LEMAIRE@lprs.fr>
2022-11-07 15:11:07 +0100

    ajout de session_start

--- src/view/entreprise/creerOffre.php
+++ src/view/entreprise/creerOffre.php
@@ -1,43 +1,40 @@
 <!doctype html>
 <html lang="fr">
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <title>Créer une offre</title>
-    <link rel="stylesheet" href="../../style/styleEntreprise.css">
+        <meta charset="utf-8">
+        <title>Créer une offre</title>
+        <link rel="stylesheet" href="../../style/styleEntreprise.css">
 
-</head>
-<body>
+    </head>
+    <body>
 
-<nav>
-    <ul>
-        <li><a href="page_accueil.php">Accueil</a></li>
-        <li><a href="../contact.php">Contact</a></li>
-        <li><a href="deconnexion.php">Déconnexion</a></li>
-    </ul>
-</nav>
+        <nav>
+            <ul>
+                <li><a href="page_accueil.php">Accueil</a></li>
+                <li><a href="../contact.php">Contact</a></li>
+                <li><a href="deconnexion.php">Déconnexion</a></li>
+            </ul>
+        </nav>
 
-<h1>Création d'un événement</h1>
+        <?php
+        session_start();
+        ?>
 
-<form action='../../traitement/evenement/traitementCreerEventEntreprise.php' method='POST'>
-    <label for='nom'><b>Nom de l'événement :</b></label>
-    <input type='text' placeholder='Nom' name='nom'  required>
+        <h1>Création d'une offre</h1>
 
-    <label for='description'><b>Description de l'événement :</b></label>
-    <textarea placeholder='Description' name='description' id="description" required></textarea>
+        <form action='../../traitement/entreprise/traitementCreerOffre.php' method='POST'>
+            <label for='titre'><b>Titre de l'offre :</b></label>
+            <input type='text' placeholder='titre' name='titre'  required>
 
-    <label for='date'><b>Date de l'événement :</b></label>
-    <input type='date' placeholder='date' name='date' required>
+            <label for='description'><b>Description de l'offre :</b></label>
+            <textarea placeholder='Description' name='description' id="description" required></textarea>
 
-    <label for='heure'><b>Heure du commencement : </b></label>
-    <input type='time' placeholder='heure' name='heure' min="07:00" max="20:00" required>
-    <small> Un événement ne peut que commencer à 7h !</small>
+            <label for='domaine'><b>Domaine :</b></label>
+            <input type='text' placeholder='domaine' name='domaine'  required>
+            <br>
+            <button type='submit' name='creerOffre' id='creerOffre'><span class="bn39span">Créer</span></button>
+        </form>
 
-    <label for='duree'><b>Durée de l'événement :</b></label>
-    <input type='time' placeholder='duree' name='duree'  required>
-    <br>
-    <button type='submit' name='creerEvenement' id='creerEvenement'><span class="bn39span">Créer</span></button>
-</form>
-
-</body>
+    </body>
 </html>
