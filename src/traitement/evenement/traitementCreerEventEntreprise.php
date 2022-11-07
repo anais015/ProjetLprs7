4e45b79c9e0c94f84e2c894f2f4182155207d175
anais015 <A.LEMAIRE@lprs.fr>
2022-11-07 14:50:26 +0100

    ajout des session_start

--- src/traitement/evenement/traitementCreerEventEntreprise.php
+++ src/traitement/evenement/traitementCreerEventEntreprise.php
@@ -1,9 +1,11 @@
 <?php
 
-//require_once "../../model/Utilisateur.php"; // D'abord instancier Utilisateur puis le reste !!
-//require_once "../../model/entreprise/Entreprise.php";
+require_once "../../model/Utilisateur.php"; // D'abord instancier Utilisateur puis le reste !!
+require_once "../../model/entreprise/Entreprise.php";
 require_once "../../model/evenement/Evenement.php";
 require_once "../../model/bdd/Bdd.php";
+
+session_start();
 
 $connexion = new Bdd();
 $bdd = $connexion->getBdd();
@@ -15,7 +17,7 @@ if(isset($_POST['creerEvenement'])) {
         'description'=> $_POST['description'],
         'debut'=> $_POST['debut'],
         'fin'=> $_POST['fin'],
-        'ref_entreprise'=>$_POST['ref_entreprise']
+        'ref_entreprise'=>$_SESSION['id_entreprise']
     ));
 
     $eve = $event->entrepriseCreerEvenement($bdd);
--- src/view/entreprise/creerEventEntreprise.php
+++ src/view/entreprise/creerEventEntreprise.php
@@ -3,41 +3,75 @@
     <head>
 
         <meta charset="utf-8">
-        <title>Organiser un événement </title>
+        <title>Organiser un événement</title>
         <link rel="stylesheet" href="../../style/styleEntreprise.css">
 
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0-next.1/esm/popper.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"></script>
     </head>
     <body>
-
-        <nav>
-            <ul>
-                <li><a href="page_accueil.php">Accueil</a></li>
-                <li><a href="modifProfil.php">Modification de profil</a></li>
-                <li><a href="../contact.php">Contact</a></li>
-                <li><a href="deconnexion.php">Déconnexion</a></li>
-            </ul>
+    <header>
+        <!--- Navbar --->
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand text-white" href="page_accueil.php"><i class="fa-duotone fa-book"></i>Accueil</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nvbCollapse" aria-controls="nvbCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="nvbCollapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item pl-1">
+                            <a class="nav-link" href="#"><i class="fa fa-home fa-fw mr-1"></i>test</a>
+                        </li>
+                        <li class="nav-item active pl-1">
+                            <a class="nav-link" href="#"><i class="fa fa-th-list fa-fw mr-1"></i>test</a>
+                        </li>
+                        <li class="nav-item pl-1">
+                            <a class="nav-link" href="#"><i class="fa fa-info-circle fa-fw mr-1"></i>test</a>
+                        </li>
+                        <li class="nav-item pl-1">
+                            <a class="nav-link" href="#"><i class="fa fa-phone fa-fw fa-rotate-180 mr-1"></i>test</a>
+                        </li>
+                        <li class="nav-item pl-1">
+                            <a class="nav-link" href="../contact.php"><i class="fa fa-user-plus fa-fw mr-1"></i>Contact</a>
+                        </li>
+                        <li class="nav-item pl-1">
+                            <a class="nav-link" href="deconnexion.php"><i class="fa fa-power-off" aria-hidden="true"></i>Déconnexion</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
+        <!--# Navbar #-->
+    </header>
 
+
+<?php
+session_start();
+var_dump($_SESSION);
+?>
         <h1>Création d'un événement</h1>
 
         <form action='../../traitement/evenement/traitementCreerEventEntreprise.php' method='POST'>
-            <label for='nom'><b>Nom de l'événement :</b></label>
-            <input type='text' placeholder='Nom' name='nom'  required>
+            <label for='nom_event'><b>Nom de l'événement :</b></label>
+            <input type='text' placeholder="Nom de l'événement" name='nom_event'  required>
 
             <label for='description'><b>Description de l'événement :</b></label>
             <textarea placeholder='Description' name='description' id="description" required></textarea>
 
-            <label for='date'><b>Date de l'événement :</b></label>
-            <input type='date' placeholder='date' name='date' required>
+            <label for='debut'><b>Date et heure de début de l'événement :</b></label>
+            <input type='datetime-local' placeholder='début' name='debut' required>
 
-            <label for='heure'><b>Heure du commencement : </b></label>
-            <input type='time' placeholder='heure' name='heure' min="07:00" max="20:00" required>
-            <small> Un événement ne peut que commencer à 7h !</small>
+            <label for='fin'><b>Date et heure de fin de l'événement : </b></label>
+            <input type='datetime-local' placeholder='fin' name='fin' required>
 
-            <label for='duree'><b>Durée de l'événement :</b></label>
-            <input type='time' placeholder='duree' name='duree'  required>
             <br>
-            <button type='submit' name='creerEvenement' id='creerEvenement'><span class="bn39span">Créer</span></button>
+            <button type='submit' name='creerEvenement' id='creerEvenement'>Créer</button>
         </form>
 
     </body>
