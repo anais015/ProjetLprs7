05c05eb2ec526f1d032ef638a2229ea219ca924c
haimy <h.le@lprs.fr>
2022-11-28 00:34:40 +0100

    fix liens de redirection

--- src/traitement/etudiant/traitementConnexion.php
+++ src/traitement/etudiant/traitementConnexion.php
@@ -16,43 +16,25 @@ if(isset($_POST['connexion'])) {
     $etudiant = new Etudiant(array(
         'email' => $_POST['email'],
     ));
-//    var_dump($etudiant);
+    //var_dump($etudiant);
     $connexion=$etudiant->connexion($bdd,$_POST['password']);
-    var_dump($connexion);
+    //var_dump($connexion);
 
-    if ($connexion) {
+    if ($connexion){
         $_SESSION['etudiant']=$connexion;
-        header("location:../../view/etudiant/accueil.php");
-    } else $erreur=true;
+        sleep(2     );
+        echo '
+                <script>
+                    window.location.href = "../../view/etudiant/accueil.php"; 
+                </script>';
+    } else {
+        echo '
+                <script>
+                    window.location.href = "../../view/connexion.php"; 
+                    alert("Erreur !");
+                </script>';
+    }
 }
 
 ?>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Connexion</title>
-</head>
-<body>
-<nav>
-    <div class="bottom-row">
-        <a href="../../view/etudiant/accueil.php">Accueil</a>
-        <a href="../../view/connexion.php">Se Connecter</a>
-        <a href="../../view/inscription.php">S'inscrire</a>
-        <a href="#">Contact</a>
-    </div>
-</nav>
-<div class="container">
-    <div class="container" id="alert"
-    <?php
-    if (!$erreur) echo 'style="display:none;"';
-    else echo 'style="display:block; background-color:#f8bdc1; text-align: center"';
-    ?>
-    <input type="hidden"> &#9888; Erreur : Veuillez réessayer.
-    <a href="../../view/connexion.php">Revenir à la page de connexion</a>
-</div>
 
-</body>
-</html>
