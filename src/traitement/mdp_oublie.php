803130866e97dc62e7523fcc0610ec5a28f7a41b
haimy <h.le@lprs.fr>
2022-12-05 12:00:53 +0100

    ajout de traitement de mot de passe oublié

--- src/traitement/mdp_oublie.php
+++ src/traitement/mdp_oublie.php
@@ -1,13 +1,29 @@
 <?php
-session_start();
-require_once "../model/bdd/Bdd.php";
-require_once "../model/Utilisateur.php";
-require_once "../model/etudiant/Etudiant.php";
+
+require_once "../model/Utilisateur.php"; // D'abord instancier Utilisateur puis le reste !!
+require_once "../model/Mail.php";
 require_once "../model/entreprise/Entreprise.php";
+require_once "../model/evenement/Evenement.php";
 require_once "../model/administrateur/Administrateur.php";
+require_once "../model/bdd/Bdd.php";
 
-$cnx = new Bdd();
-$bdd = $cnx->getBdd();
-var_dump($_POST);
+$bdd = new Bdd();
+$cnx = $bdd->getBdd();
 
+if ($_POST['selectIdentity'] === 'etudiant'){
+    $etudiant = new Etudiant(array(
+        "email"=>$_POST['email']
+    ));
+    $etudiant->newCode($cnx);
+}elseif ($_POST['selectIdentity'] === 'entreprise'){
+    $entreprise = new Entreprise(array(
+        "email"=>$_POST['email']
+    ));
+    $entreprise->newCode($cnx);
+}elseif($_POST['selectIdentity'] === 'entreprise') {
+    $admin = new Administrateur(array(
+        "email" => $_POST['email']
+    ));
+    $admin->newCode($cnx);
+}
 ?>
